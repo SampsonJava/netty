a4fc1723b10edd191ba00e34f521ac2e2c8a62e5
Sampson <379850992@qq.com>
2022-05-26 22:00:23 +0800

    test

--- example/src/main/java/io/netty/example/echo/EchoClient.java
+++ example/src/main/java/io/netty/example/echo/EchoClient.java
@@ -29,6 +29,7 @@ import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
 /**
+ * 加个注释玩玩了
  * Sends one message when a connection is open and echoes back any received
  * data to the server.  Simply put, the echo client initiates the ping-pong
  * traffic between the echo client and server by sending the first message to
